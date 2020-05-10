8dafc12f67c2d9ca894f1dfb6dd691a4080e76b9
Minajeanne <albrown07@mac.com>
2020-05-10 10:52:24 -0400

    init tailwind config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './styles/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="">
+      <header className="b">
         Plan Your Escape
       </header>
     </div>
