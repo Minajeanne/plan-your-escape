28877cc45a864a9bb0f42c0badb081de6864dfb1
Minajeanne <albrown07@mac.com>
2020-05-13 08:31:37 -0400

    updated app name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./styles/main.css";
-// import background from './images/ivana-cajina-unsplash.jpg';
+// import camping from './images/undraw_camping_noc8.svg';
 import backgroundPattern from './images/topography.svg';
 
 function App() {
   return (
     <div>
       <div class="px-6 py-4">
-        <div class="text-center font-sans font-bold text-red-800 text-5xl mb-2">Plan Your Escape</div>
-        <p class="text-white text-center text-3xl text-base">
+        {/* <img className="" src={ camping } alt="camping tent" /> */}
+        <div class="align-middle text-center font-sans font-bold text-green-900 text-5xl mb-2">Plan Your Escape</div>
+        <p class="text-white text-center text-3xl text-base p-2">
           I don't know about you, but I need to get out of here.
         </p>
       </div>
