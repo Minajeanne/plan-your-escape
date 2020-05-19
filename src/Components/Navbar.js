745e76c36c32c332d41a022650e5c66e088ed532
Minajeanne <albrown07@mac.com>
2020-05-19 13:09:01 -0400

    menu function opens list

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -60,19 +60,24 @@ class Navbar extends React.Component {
               <p id="status"></p>
               <a id="map-link" target="_blank"></a>
             </div>
-            <button onClick={this.handleClick} type="button" class="block text-gray-700 hover:text-gray-400 focus:text-white focus:outline-none">
+            <button onClick={this.handleClick} type="button" class="block text-gray-700 hover:text-gray-400 focus:text-gray-500 focus:outline-none">
               <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
-                <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+                {this.state.isOpen ?
+                  <path fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+                :
+                <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+                }
               </svg>
             </button>
           </div>
         </div>
-        <div className="px-2 pt-2 pb-4">
-          <a href="#" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">List your property</a>
-          <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Trips</a>
-          <a href="#" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Messages</a>
-        </div>
+        {this.state.isOpen ?
+          <div className="px-2 pt-2 pb-4">
+            <a href="#" class="block px-2 py-1 text-gray-900 font-semibold rounded hover:bg-gray-500">List your property</a>
+            <a href="#" class="mt-1 block px-2 py-1 text-gray-900 font-semibold rounded hover:bg-gray-500">Trips</a>
+            <a href="#" class="mt-1 block px-2 py-1 text-gray-900 font-semibold rounded hover:bg-gray-500">Messages</a>
+          </div>
+        : null }
       </header>
     );
   }
